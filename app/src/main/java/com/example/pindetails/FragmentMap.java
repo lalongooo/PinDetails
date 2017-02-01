e049ca931a6581ea28f1cc82e4c90eb217706650
lalongooo <hdez.jeduardo@gmail.com>
2017-01-31 20:04:21 -0600

    Add Marker on long press.

--- app/src/main/java/com/example/pindetails/FragmentMap.java
+++ app/src/main/java/com/example/pindetails/FragmentMap.java
@@ -11,6 +11,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by jorge.hernandez on 1/31/2017.
@@ -18,13 +19,14 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class FragmentMap extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
-    private MapView mapView;
+    private MapView mMmapView;
+    private GoogleMap mGoogleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View fragmentView = inflater.inflate(R.layout.fragment_map, container, false);
-        mapView = (MapView) fragmentView.findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
+        mMmapView = (MapView) fragmentView.findViewById(R.id.mapView);
+        mMmapView.onCreate(savedInstanceState);
 
         return fragmentView;
     }
@@ -32,50 +34,50 @@ public class FragmentMap extends Fragment implements OnMapReadyCallback, GoogleM
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mapView.getMapAsync(this);
-    }
-
-
-    @Override
-    public void onMapLongClick(LatLng latLng) {
-        Log.i(getClass().getCanonicalName(), "Map has been long clicked!");
+        mMmapView.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        Log.i(getClass().getCanonicalName(), "Map is ready!");
+        mGoogleMap = googleMap;
         googleMap.setOnMapLongClickListener(this);
     }
 
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        mGoogleMap.clear();
+        mGoogleMap.addMarker(new MarkerOptions().position(latLng).title("Hi!"));
+        Log.i(getClass().getCanonicalName(), "Map has been long clicked!");
+    }
 
     @Override
     public void onResume() {
         super.onResume();
-        mapView.onResume();
+        mMmapView.onResume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        mapView.onPause();
+        mMmapView.onPause();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mapView.onDestroy();
+        mMmapView.onDestroy();
     }
 
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-        mapView.onLowMemory();
+        mMmapView.onLowMemory();
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
+        mMmapView.onSaveInstanceState(outState);
     }
 
 }
